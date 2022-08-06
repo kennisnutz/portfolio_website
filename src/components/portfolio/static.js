e6f520a8cbece7d27754649bca52fefb13df28ea
kennisnutz <kenneth.hotson83@gmail.com>
2022-08-06 02:17:35 +0100

    added new project to portfolio list

--- src/components/portfolio/static.js
+++ src/components/portfolio/static.js
@@ -1,28 +1,36 @@
 import Portfolio1 from '../../assets/portfolio1.jpg'
 import Portfolio2 from '../../assets/portfolio2.jpg'
 import Portfolio3 from '../../assets/portfolio3.png'
+import Portfolio4 from '../../assets/portfolio4.png'
 
-const data=[
+const data = [
     {
-        id:1,
-        image:Portfolio1 ,
+        id: 1,
+        image: Portfolio1,
         title: "NFT Collection minting dapp",
         github: "https://github.com/kennisnutz/KUCOIN-KONGS",
         demo: "https://frozen-cove-50661.herokuapp.com/"
     },
     {
-        id:2,
-        image:Portfolio2 ,
+        id: 2,
+        image: Portfolio2,
         title: "Web3 twitter clone dapp",
-        github:"https://github.com/kennisnutz/NFTwitta",
-        demo: "https://nfttwitta-kennisnutz.vercel.app/" 
+        github: "https://github.com/kennisnutz/NFTwitta",
+        demo: "https://nfttwitta-kennisnutz.vercel.app/"
     },
     {
-        id:3,
-        image:Portfolio3 ,
-        title:" Visney art minter dapp",
+        id: 3,
+        image: Portfolio3,
+        title: " Visney art minter dapp",
         github: "https://github.com/kennisnutz/Visney",
         demo: "https://www.visney.org/"
+    },
+    {
+        id: 4,
+        image: Portfolio4,
+        title: " Fan footy nft mart",
+        github: "https://github.com/kennisnutz/nftMart",
+        demo: "https://footy-fans-nfts.web.app/"
     }
 ]
 
