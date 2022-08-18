b214bac55d0028a48f4b4cb8e3659178ebfe2caa
kennisnutz <kenneth.hotson83@gmail.com>
2022-08-18 17:49:51 +0000

    added new project to portfolio list

--- src/components/portfolio/static.js
+++ src/components/portfolio/static.js
@@ -2,15 +2,17 @@ import Portfolio1 from '../../assets/portfolio1.jpg'
 import Portfolio2 from '../../assets/portfolio2.jpg'
 import Portfolio3 from '../../assets/portfolio3.png'
 import Portfolio4 from '../../assets/portfolio4.png'
+import Portfolio6 from '../../assets/portfolio6.png'
 
 const data = [
     {
         id: 1,
-        image: Portfolio1,
-        title: "NFT Collection minting dapp",
-        github: "https://github.com/kennisnutz/KUCOIN-KONGS",
-        demo: "https://frozen-cove-50661.herokuapp.com/"
+        image: Portfolio6,
+        title: " FFLeague NFT",
+        github: "https://github.com/kennisnutz/ffleaguemart",
+        demo: "https://fantasyfootballnftmart.web.app/"
     },
+   
     {
         id: 2,
         image: Portfolio2,
@@ -31,7 +33,15 @@ const data = [
         title: " Fan footy nft mart",
         github: "https://github.com/kennisnutz/nftMart",
         demo: "https://footy-fans-nfts.web.app/"
-    }
+    },
+    {
+        id: 5,
+        image: Portfolio1,
+        title: "NFT Collection minting dapp",
+        github: "https://github.com/kennisnutz/KUCOIN-KONGS",
+        demo: "https://frozen-cove-50661.herokuapp.com/"
+    },
+
 ]
 
 export default data;
