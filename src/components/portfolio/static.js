c23b7eadb6b90ab73da68333efaed6b798fac9a1
kennisnutz <kenneth.hotson83@gmail.com>
2022-08-20 12:42:00 +0000

    added new project to portfolio list

--- src/components/portfolio/static.js
+++ src/components/portfolio/static.js
@@ -1,47 +1,54 @@
-import Portfolio1 from '../../assets/portfolio1.jpg'
-import Portfolio2 from '../../assets/portfolio2.jpg'
-import Portfolio3 from '../../assets/portfolio3.png'
-import Portfolio4 from '../../assets/portfolio4.png'
-import Portfolio6 from '../../assets/portfolio6.png'
+import Portfolio1 from "../../assets/portfolio1.jpg";
+import Portfolio2 from "../../assets/portfolio2.jpg";
+import Portfolio3 from "../../assets/portfolio3.png";
+import Portfolio4 from "../../assets/portfolio4.png";
+import Portfolio6 from "../../assets/portfolio6.png";
+import Portfolio7 from "../../assets/portfolio7.png";
 
 const data = [
-    {
-        id: 1,
-        image: Portfolio6,
-        title: " FFLeague NFT",
-        github: "https://github.com/kennisnutz/ffleaguemart",
-        demo: "https://fantasyfootballnftmart.web.app/"
-    },
-   
-    {
-        id: 2,
-        image: Portfolio2,
-        title: "Web3 twitter clone dapp",
-        github: "https://github.com/kennisnutz/NFTwitta",
-        demo: "https://nfttwitta-kennisnutz.vercel.app/"
-    },
-    {
-        id: 3,
-        image: Portfolio3,
-        title: " Visney art minter dapp",
-        github: "https://github.com/kennisnutz/Visney",
-        demo: "https://www.visney.org/"
-    },
-    {
-        id: 4,
-        image: Portfolio4,
-        title: " Fan footy nft mart",
-        github: "https://github.com/kennisnutz/nftMart",
-        demo: "https://footy-fans-nfts.web.app/"
-    },
-    {
-        id: 5,
-        image: Portfolio1,
-        title: "NFT Collection minting dapp",
-        github: "https://github.com/kennisnutz/KUCOIN-KONGS",
-        demo: "https://frozen-cove-50661.herokuapp.com/"
-    },
+  {
+    id: 1,
+    image: Portfolio6,
+    title: " FFLeague NFT",
+    github: "https://github.com/kennisnutz/ffleaguemart",
+    demo: "https://fantasyfootballnftmart.web.app/",
+  },
 
-]
+  {
+    id: 2,
+    image: Portfolio2,
+    title: "Web3 twitter clone dapp",
+    github: "https://github.com/kennisnutz/NFTwitta",
+    demo: "https://nfttwitta-kennisnutz.vercel.app/",
+  },
+  {
+    id: 3,
+    image: Portfolio7,
+    title: "Near Safe coin stats",
+    github: "https://github.com/kennisnutz/near-safe-coin",
+    demo: "https://near-safe-coin.netlify.app/",
+  },
+  {
+    id: 4,
+    image: Portfolio4,
+    title: " Fan footy nft mart",
+    github: "https://github.com/kennisnutz/nftMart",
+    demo: "https://footy-fans-nfts.web.app/",
+  },
+  {
+    id: 5,
+    image: Portfolio1,
+    title: "NFT Collection minting dapp",
+    github: "https://github.com/kennisnutz/KUCOIN-KONGS",
+    demo: "https://frozen-cove-50661.herokuapp.com/",
+  },
+  {
+    id: 6,
+    image: Portfolio3,
+    title: " Visney art minter dapp",
+    github: "https://github.com/kennisnutz/Visney",
+    demo: "https://www.visney.org/",
+  },
+];
 
 export default data;
